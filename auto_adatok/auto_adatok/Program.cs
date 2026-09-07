e6e0a2c0d4c33f882ae99a7213d552d10865d112
Lukics Máté 789 <lukmat789@logiker.hu>
2026-09-07 12:20:54 +0200

    autoklista készítése

--- auto_adatok/auto_adatok/Program.cs
+++ auto_adatok/auto_adatok/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,6 +56,70 @@ namespace auto_adatok
 
                 }
 
+            }
+        }
+        static List<autok> autokLista()
+        {
+            var lista = new List<autok>();
+
+            using (StreamReader sr = new StreamReader("auto_adatok.csv"))
+            {   
+                sr.ReadLine ();
+                try
+                {
+                    using (MySqlConnection conn = new MySqlConnection())
+                    {
+                        conn.ConnectionString = "Server=127.0.0.1;Port=3307;Database=auto_adatok;Uid=root";
+                        conn.Open();
+
+                        using (MySqlCommand cmd = new MySqlCommand())
+                        {
+                            while (!sr.EndOfStream) 
+                            {
+                                var srsplit = sr.ReadLine();
+                                var row = srsplit.Split(',');
+                                var ID = row[0];
+                                var márka = row[1];
+                                var tipus = row[2];
+                                var üzemanyag = row[3];
+                                var teljesítmény = row[4];
+                                var vételár = row[5];
+                                var gyártásiÉv = row[6];
+                                var átlagosCo2 = row[7];
+
+                                int.TryParse(ID, out int IDD);
+                                márka.ToString();
+                                tipus.ToString();
+                                üzemanyag.ToString();
+                                int.TryParse(teljesítmény, out int teljesítményINT);
+                                int.TryParse(vételár, out int vételárINT);
+                                short.TryParse(gyártásiÉv, out short gyártásiévINT);
+                                int.TryParse(átlagosCo2, out int átlagosCo2INT);
+
+
+                                cmd.CommandText = "SELECT MárkaID FROM Márka WHERE @Márkaa = MárkaNév";
+                                cmd.Parameters["@Márkaa"].Value = márka;
+                                var cmdexecute = cmd.ExecuteReader();
+                                bool found = cmdexecute.Read();
+                                if (!found)
+                                {
+                                    cmd.CommandText = "INSERT INTO Márka(MárkaNév) VALUES(@márkaérték)";
+                                    cmd.Parameters["@márkaérték"].Value = márka;
+                                    cmd.ExecuteNonQuery();
+                                }
+
+
+
+                            }
+                        }
+                    }
+                }
+                    catch (Exception)
+                {
+
+                    throw;
+                }
+                return lista;
             }
         }
 
---
+++ auto_adatok/auto_adatok/class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auto_adatok
+{
+    internal class autok
+    {
+        public string Márka {  get; set; }
+
+        public string Típus { get; set; }
+        public string Üzemanyag { get; set; }
+        public int Teljesítmény_LE {  get; set; }
+        public int Vételár_EUR {  get; set; }
+        public short Gyártási_év {  get; set; }
+        public int Átlagos_CO2_g_km { get; set; }
+    }
+}
